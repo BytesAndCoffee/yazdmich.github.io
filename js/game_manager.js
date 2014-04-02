1fe8adc082b4f6ca1e15d6371c7fc5bfc09e9986
yazdmich <yazdmich@gmail.com>
2014-04-02 00:08:53 -0400

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -1,86 +1,88 @@
 function GameManager(size, InputManager, Actuator) {
-  this.size         = size; // Size of the grid
-  this.inputManager = new InputManager;
-  this.actuator     = new Actuator;
+    this.size = size; // Size of the grid
+    this.inputManager = new InputManager;
+    this.actuator = new Actuator;
 
-  this.running      = false;
+    this.running = false;
 
-  this.inputManager.on("move", this.move.bind(this));
-  this.inputManager.on("restart", this.restart.bind(this));
+    this.inputManager.on("move", this.move.bind(this));
+    this.inputManager.on("restart", this.restart.bind(this));
 
-  this.inputManager.on('think', function() {
-    var best = this.ai.getBest();
-    this.actuator.showHint(best.move);
-  }.bind(this));
+    this.inputManager.on('think', function () {
+        var best = this.ai.getBest();
+        this.actuator.showHint(best.move);
+    }.bind(this));
 
 
-  this.inputManager.on('run', function() {
-    if (this.running) {
-      this.running = false;
-      this.actuator.setRunButton('Auto-run');
-    } else {
-      this.running = true;
-      this.run()
-      this.actuator.setRunButton('Stop');
-    }
-  }.bind(this));
+    this.inputManager.on('run', function () {
+        if (this.running) {
+            this.running = false;
+            this.actuator.setRunButton('Auto-run');
+        }
+        else {
+            this.running = true;
+            this.run()
+            this.actuator.setRunButton('Stop');
+        }
+    }.bind(this));
 
-  this.setup();
+    this.setup();
 }
 
 // Restart the game
 GameManager.prototype.restart = function () {
-  this.actuator.restart();
-  this.running = false;
-  this.actuator.setRunButton('Auto-run');
-  this.setup();
+    this.actuator.restart();
+    this.running = false;
+    this.actuator.setRunButton('Auto-run');
+    this.setup();
 };
 
 // Set up the game
 GameManager.prototype.setup = function () {
-  this.grid         = new Grid(this.size);
-  this.grid.addStartTiles();
+    this.grid = new Grid(this.size);
+    this.grid.addStartTiles();
 
-  this.ai           = new AI(this.grid);
+    this.ai = new AI(this.grid);
 
-  this.score        = 0;
-  this.over         = false;
-  this.won          = false;
+    this.score = 0;
+    this.over = false;
+    this.won = false;
 
-  // Update the actuator
-  this.actuate();
+    // Update the actuator
+    this.actuate();
 };
 
 
 // Sends the updated grid to the actuator
 GameManager.prototype.actuate = function () {
-  this.actuator.actuate(this.grid, {
-    score: this.score,
-    over:  this.over,
-    won:   this.won
-  });
+    this.actuator.actuate(this.grid, {
+        score: this.score,
+        over: this.over,
+        won: this.won
+    });
 };
 
 // makes a given move and updates state
-GameManager.prototype.move = function(direction) {
-  var result = this.grid.move(direction);
-  this.score += result.score;
+GameManager.prototype.move = function (direction) {
+    var result = this.grid.move(direction);
+    this.score += result.score;
 
-  if (!result.won) {
-    if (result.moved) {
-      this.grid.computerMove();
+    if (!result.won) {
+        if (result.moved) {
+            this.grid.computerMove();
+        }
     }
-  } else {
-    this.won = true;
-  }
+    else {
+        this.won = true;
+    }
 
-  //console.log(this.grid.valueSum());
+    //console.log(this.grid.valueSum());
 
-  if (!this.grid.movesAvailable()) {
-    this.over = true; // Game over!
-  }
+    if (!this.grid.movesAvailable()) {
+        this.over = true; // Game over!
+    }
 
-  this.actuate();
+    this.actuate();
 }
 
 // moves continuously until game is over
@@ -98,18 +100,18 @@ GameManager.prototype.run = function() {
 }
 */
 
-GameManager.prototype.run = function() {
-  this.keys = [0, 1, 2, 3];
-  setTimeout(function(){
-    this.keys.forEach(function(i){
-      this.move(i);
+GameManager.prototype.run = function () {
+    this.keys = [0, 1, 2, 3];
+    this.keys.forEach(function (i) {
+        setTimeout(function (i) {
+            this.move(i);
+        }.bind(this), animationDelay);
     }.bind(this));
-  }.bind(this), animationDelay);
-  var timeout = animationDelay;
-  if (this.running && !this.over && !this.won) {
-    var self = this;
-    setTimeout(function(){
-      self.run();
-    }, timeout);
-  }
+    var timeout = animationDelay;
+    if (this.running && !this.over && !this.won) {
+        var self = this;
+        setTimeout(function () {
+            self.run();
+        }, timeout);
+    }
 }
