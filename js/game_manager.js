590bf0423e53feaadf4fa91dd230098784077614
yazdmich <yazdmich@gmail.com>
2014-04-02 00:14:44 -0400

    Update game_manager.js

--- js/game_manager.js
+++ js/game_manager.js
@@ -105,7 +105,7 @@ GameManager.prototype.run = function () {
     this.keys.forEach(function (i) {
         setTimeout(function (i) {
             this.move(i);
-        }.bind(this), animationDelay);
+        }, animationDelay);
     }.bind(this));
     var timeout = animationDelay;
     if (this.running && !this.over && !this.won) {
